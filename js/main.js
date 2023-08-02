164d98eaa066aa791123a3fc7615a30a8f25e434
Scoreapps <87309867+Scoreapps@users.noreply.github.com>
2023-08-02 13:07:31 +0200

    Update with arrow functions

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
 window.onload = function() {
 
-    function setValue(newValue)
-    {
-        document.querySelector('#result').innerHTML = newValue;
-    }
-    
+    let setValue = (newValue) => document.querySelector('#result').innerHTML = newValue;
     let valueCounter = Number(document.querySelector('#result').innerHTML);  
     
-    document.querySelector('#btnIncrement').addEventListener("click", function()
-    {
+    document.querySelector('#btnIncrement').addEventListener("click", () => {
+        
         if(valueCounter !== 10){
             setValue(++valueCounter);
         }
+
     });
     
-    document.querySelector('#btnDecrement').addEventListener("click", function()
-    {
+    document.querySelector('#btnDecrement').addEventListener("click", () => {
+
         if(valueCounter !== 0){
             setValue(--valueCounter);
         }
+
     });
     
-    document.querySelector('#btnRestart').addEventListener("click", function()
-    {
+    document.querySelector('#btnRestart').addEventListener("click", () => {
+
         if(valueCounter !== 0){
             valueCounter = 0;
             setValue(0);
         }
+
     });
 
 }
